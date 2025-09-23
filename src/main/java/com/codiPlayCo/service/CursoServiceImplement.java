db8ed9a9ef47661b08da1bfa01436c2391804ac5
juanchobaez <juandavidbaezobregon@gmail.com>
2025-09-23 13:04:26 -0500

    actualizacion

--- src/main/java/com/codiPlayCo/service/CursoServiceImplement.java
+++ src/main/java/com/codiPlayCo/service/CursoServiceImplement.java
@@ -1,21 +1,46 @@
 package com.codiPlayCo.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.codiPlayCo.model.Curso;
 import com.codiPlayCo.repository.ICursoRepository;
+
 @Service
-public class CursoServiceImplement {
+public class CursoServiceImplement implements ICursoService {
 
 	@Autowired
 	private ICursoRepository cursoRepository;
 
+	
+	@Override
 	public List<Curso> findAll() {
 		return cursoRepository.findAll();
 	}
 
+	@Override
+	public Curso save(Curso curso) {
+		return cursoRepository.save(curso);
+	}
 
+	@Override
+	public Curso update(Curso curso) {
+		return cursoRepository.save(curso);
+	}
+	
+	
+
+	@Override
+	public void delete(Integer id) {
+		cursoRepository.deleteById(id);
+	}
+
+	@Override
+	public Optional<Curso> get(Integer id) {
+		// TODO Auto-generated method stub
+		return Optional.empty();
+	}
 }
