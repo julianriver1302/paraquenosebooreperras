b3d4ff7093cd552adc0f9b090d8c49c78e128cac
Alejandro Amado <amadofuentesdavidalejandro5@gmail.com>
2025-11-24 14:42:11 -0500

    Riverosmk

--- src/main/java/com/codiPlayCo/controller/CursoTrilogia1Controller.java
+++ src/main/java/com/codiPlayCo/controller/CursoTrilogia1Controller.java
@@ -1,7 +1,9 @@
 package com.codiPlayCo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
